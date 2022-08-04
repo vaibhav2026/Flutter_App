a59e9d712e6b5b181e70c0479d02a41573d92fe4
vaibhav2026 <kalevaibhav2303@gmail.com>
2022-08-04 23:23:09 +0530

    first

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+//void main() {
+//  runApp(MyApp());
+//}
+
+void main() =>
+    runApp(MyApp()); // this is just an alternate syntax to that nothing else.
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var questions = ['What\'s your name?',
+     'What\'s did you actually want to do?',
+     ];
+     return MaterialApp(
+       home: Scaffold(
+         appBar: AppBar(
+           title: Text('My First App'),
+           ),
+           body: Column(
+             children: [
+               Text('The question is'),
+               RaisedButton(child: Text('Answer 2'), onPressed: null),
+               RaisedButton(child: Text('Answer 3'), onPressed: null),
+               RaisedButton(child: Text('Answer 1'), onPressed: null),
+             ],
+        ),
+     );
+    }
+}
